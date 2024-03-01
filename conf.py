27ea0ab75308e7a126256603e2b214007315284e
Hillel <h@hillelwayne.com>
2024-03-01 11:51:56 -0600

    Update to a new version of sphinx, maybe

--- conf.py
+++ conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 default_role = "any"
 highlight_language = "alloy"
